eb559ddde3ca57726ab7f43e88ffd39b293ffa28
Abdullah <abdullahriaz0753@gmail.com>
2026-07-09 09:43:05 +0500

    add new  function

--- models/diesel_model.py
+++ models/diesel_model.py
@@ -5,7 +5,7 @@ Diesel Model
 Handles diesel purchase entries per trip (Irani / Pakistani / Other).
 """
 
-from database.db_manager imp get_connection
+from database.db_manager  get_connection
 
 
 def add_diesel(trip_id, diesel_type, liters, rate_per_liter, total_cost, pump_location, diesel_date):
